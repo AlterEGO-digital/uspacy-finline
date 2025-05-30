3590611733d18d8e979cc64e7c671cbd8d535113
dprokoptsov <dprokoptsov.codica@gmail.com>
2025-05-30 13:15:01 +0300

    feat: add data source hooks for payment

--- src/hooks/usePaymentSourceData.ts
+++ src/hooks/usePaymentSourceData.ts
@@ -1,9 +1,19 @@
+import { useMemo } from 'react';
+
 import { useGetCurrencyListQuery, useGetPaymentAccountsListQuery, useGetRecieptDeliveryListQuery } from '../store/reducers/payment/api-slice';
+import { useDeal } from './useDeal';
+
+// TODO: delete once BE is ready. Now it is mok data for testing
+const a = [
+	{ id: '1', label: 'Fop' },
+	{ id: '2', label: 'TOB' },
+];
 
 export const usePaymentSourceData = () => {
 	const accauntPayment = useGetPaymentAccountsListQuery();
 	const currencyPayment = useGetCurrencyListQuery();
 	const recieptDelivery = useGetRecieptDeliveryListQuery();
+	const { deal } = useDeal();
 
 	const isLoading =
 		accauntPayment.isLoading ||
@@ -27,9 +37,27 @@ export const usePaymentSourceData = () => {
 		recieptDelivery.error ||
 		recieptDelivery.error;
 
+	const emails = useMemo(
+		() =>
+			deal?.contacts
+				?.map((contact) => contact.email.map((email) => ({ id: email.value, label: email.value, owner: contact.title })))
+				.flat()
+				.filter((email) => !!email.label) ?? [],
+		[deal],
+	);
+	const phones = useMemo(
+		() =>
+			deal?.contacts?.map((contact) => ({ id: contact.phone, label: contact.phone, owner: contact.title })).filter((phone) => !!phone.label) ??
+			[],
+		[deal],
+	);
+
 	return {
-		accounts: accauntPayment.data ?? [],
-		currency: currencyPayment.data ?? [],
+		accounts: accauntPayment.data ?? a,
+		currencies: currencyPayment.data ?? [],
+		recieptDeliveryTransports: recieptDelivery.data ?? [],
+		emails,
+		phones,
 		isLoading,
 		isError,
 		error,
