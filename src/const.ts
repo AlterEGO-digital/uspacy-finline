cc3e1fcbbfc9169e3b80561f55d90fa0292b9b7c
dprokoptsov <dprokoptsov.codica@gmail.com>
2025-06-06 21:21:08 +0300

    fix: api endpoints

--- src/const.ts
+++ src/const.ts
@@ -1,10 +1,13 @@
 export const isStage = ['https://stage3.staging.uspacy.tech', 'http://localhost:8080'].includes(window.location.origin);
-export const API_URL = isStage ? '' : '';
+export const API_URL = isStage ? 'http://test-finline.alterego.biz.ua' : 'https://finline.alterego.digital/finline';
+
+const API_PREFIX = 'Laravel/v1';
 
 export const API_ENDPOINT = {
-	generatePaymentLink: () => '/link/generate',
-	paymentAccounts: () => '/accounts',
-	getSettings: () => '/settings',
-	saveSettings: () => '/accounts/store',
-	deletePaymentAccount: (id: string) => `/accounts/destroy/${id}`,
+	generatePaymentLink: () => `${API_PREFIX}/link/generate`,
+	paymentAccounts: () => `${API_PREFIX}/accounts`,
+	saveSettings: () => `${API_PREFIX}/accounts/store`,
+	dealStatus: () => `${API_PREFIX}/pay-status`,
+	saveDealStatus: () => `${API_PREFIX}/pay-status/store`,
+	deletePaymentAccount: (id: string) => `${API_PREFIX}/accounts/destroy/${id}`,
 };
