3d7f954b59c6345935f0919a7b5e7ae9fc937e64
dprokoptsov <dprokoptsov.codica@gmail.com>
2025-06-18 18:48:59 +0300

    fix: remove mock data

--- src/helpers/errors.ts
+++ src/helpers/errors.ts
@@ -6,14 +6,16 @@ export const getErrorMessage = (err: unknown) => {
 	if (isString(err)) {
 		return { message: err };
 	}
+
+	if (isAxiosError(err)) {
+		return { message: err.response.data?.message || err.response?.data || 'Something went wrong' };
+	}
 	if (err instanceof Error) {
 		return { message: err.message ?? '' };
 	}
+
 	if (isObject(err) && 'message' in err) {
 		return { message: err.message as string };
-	}
-	if (isAxiosError(err)) {
-		return { message: err.response.data?.message || err.response?.data };
 	}
 
 	return { message: 'Something went wrong' };
