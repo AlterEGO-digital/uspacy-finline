c663e9ba4722aca5598e5deb37ac3052932fa4a4
dprokoptsov <dprokoptsov.codica@gmail.com>
2025-06-23 15:15:33 +0300

    debug on stage

--- src/store/reducers/app/selectors.ts
+++ src/store/reducers/app/selectors.ts
@@ -4,7 +4,7 @@ import { RootState } from '../..';
 export const selectDeal = (state: RootState): IDeal | null => {
 	const deal = state.app.deal;
 
-	if (!deal?.amount && !deal?.contacts && !deal?.id) {
+	if (!deal?.id) {
 		return null;
 	}
 
