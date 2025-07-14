bf400d86162fa9f9ec33a37ed1f8be6fbced222b
dprokoptsov <dprokoptsov.codica@gmail.com>
2025-07-14 21:58:02 +0300

    feat: integrate useIntegrationToken into Providers and SettingsWrap for token management

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -1,29 +1,45 @@
 import { ThemeProvider } from '@emotion/react';
 import { SnackbarProvider } from 'notistack';
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useLayoutEffect } from 'react';
 
 import LocalizationProvider from './context/Localization';
 import { StoreProvider } from './context/StoreProvider';
 import UserSettingsProvider from './context/UserSettings';
+import { useIntegrationToken } from './hooks/useIntegrationToken';
 import { IUserSettings } from './models/userSettings';
 import { theme } from './static/styles/theme';
 
 interface Props {
 	children: ReactNode;
 	userSettings?: IUserSettings;
+	integrationToken?: string;
 }
-const Providers: React.FC<Props> = ({ children, userSettings }) => {
+const Providers: React.FC<Props> = ({ children, userSettings, integrationToken }) => {
 	return (
 		<ThemeProvider theme={theme}>
 			<UserSettingsProvider userSettings={userSettings}>
 				<StoreProvider>
-					<LocalizationProvider>
-						<SnackbarProvider autoHideDuration={2000}>{children}</SnackbarProvider>
-					</LocalizationProvider>
+					<TokenProvider integrationToken={integrationToken}>
+						<LocalizationProvider>
+							<SnackbarProvider autoHideDuration={2000}>{children}</SnackbarProvider>
+						</LocalizationProvider>
+					</TokenProvider>
 				</StoreProvider>
 			</UserSettingsProvider>
 		</ThemeProvider>
 	);
 };
+
+function TokenProvider({ integrationToken, children }: { integrationToken: string; children: React.ReactNode }) {
+	const { setToken } = useIntegrationToken();
+
+	useLayoutEffect(() => {
+		if (integrationToken) {
+			setToken(integrationToken);
+		}
+	}, [integrationToken]);
+
+	return <>{children}</>;
+}
 
 export default Providers;
