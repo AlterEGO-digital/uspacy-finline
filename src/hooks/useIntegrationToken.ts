ace12817c34298ce72fd27b467e7d0236622db84
dprokoptsov <dprokoptsov.codica@gmail.com>
2025-07-14 21:23:08 +0300

    fix: add null check for integrationToken before setting it

--- src/hooks/useIntegrationToken.ts
+++ src/hooks/useIntegrationToken.ts
@@ -1,12 +1,11 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { setIntegrationToken } from '../helpers/db';
-import { useAppSelector } from '../store';
 import { setIntegrationToken as setIntegrationTokenAction } from '../store/reducers/app/actions';
 import { selectIntegrationToken } from '../store/reducers/app/selectors';
 
 export const useIntegrationToken = () => {
-	const token = useAppSelector(selectIntegrationToken);
+	const token = useSelector(selectIntegrationToken);
 	const dispatch = useDispatch();
 
 	const setToken = (t: string) => {
