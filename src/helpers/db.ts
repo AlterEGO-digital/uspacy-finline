dbcf1ae32becb04d8d2e6bd51f78a62b91fc4ad1
dprokoptsov <dprokoptsov.codica@gmail.com>
2025-07-15 10:44:11 +0300

    Revert "Merge pull request #29 from AlterEGO-digital/hotfix/909_integration_token"
    This reverts commit a96c4f306a61e53be0bdc6bbebd4edebf9aef7ac, reversing
    changes made to 315c52471e7f114f52c6a243c25ebe00747c3e0f.

--- src/helpers/db.ts
+++ src/helpers/db.ts
@@ -27,10 +27,14 @@ export const getToken = async (): Promise<string> => {
 };
 
 export const setIntegrationToken = async (token: string): Promise<string> => {
+	// eslint-disable-next-line no-console
+	console.log('SET finline:integration_token');
 	return table.setItem('finline:integration_token', token);
 };
 export const getIntegrationToken = async (): Promise<string> => {
 	const token = await table.getItem<string>('finline:integration_token');
+	// eslint-disable-next-line no-console
+	console.log('GET finline:integration_token', token);
 
 	return token;
 };
